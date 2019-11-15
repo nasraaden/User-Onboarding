9a65270f96794bdded724812d81faab6679d8818
Nasra <nasraaden@Nasras-MacBook-Pro.local>
2019-11-14 16:31:58 -0800

    MVP

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Form from "./components/Form"
+// import User from "./components/User"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sign Up</h1>
+      <Form/>
+      {/* <User /> */}
+    </div>
+  );
+}
+
+export default App;
